16948e2169bfe41778cb94fbec5cf94a3cc438ba
red2fred2 <admin@red2fred2.com>
2019-04-11 21:18:07 -0500

    skeleton fro settings manager

---
+++ Settings.lua
@@ -0,0 +1,22 @@
+--------------------IMPORT--------------------
+
+require 'Class'
+
+--------------------OBJECT--------------------
+
+Settings = Class:new()
+
+--------------------VARIABLES--------------------
+
+
+--------------------CONSTRUCTOR--------------------
+
+function Settings:new()
+  return self
+end
+
+--------------------FUNCTIONS--------------------
+
+function Settings:update(options)
+
+end
