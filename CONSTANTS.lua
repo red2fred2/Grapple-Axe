2fd41d52c0fafce5d237f6a047fda5de76fea83b
red2fred2 <admin@red2fred2.com>
2019-04-11 21:50:30 -0500

    added constants file to make them easier to change

---
+++ CONSTANTS.lua
@@ -0,0 +1,4 @@
+DEBUG = true
+meterPX = 64
+g = 9.81
+gamepadMapFile = 'gamepad-mappings'
--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@
 require 'Game'
 require 'Object'
 require 'Graphics'
+require 'InputHandler'
 
 --------------------CONSTANTS--------------------
 
@@ -16,39 +17,35 @@ g = 9.81
 function love.load()
   game = Game:new()
 
+  --world init
   love.physics.setMeter(meterPX)
-
   world = love.physics.newWorld(0, meterPX*g, true)
 
+  --object init
   Object('static', 'ground', 0, 600, {0, 0, 650,0, 650,50, 0,50}, {color={0.28, 0.63, 0.05}})
   Object('dynamic', 'triangle', 325, 325, {20,0, 40,40, 0,40}, {density=1, color={0.76, 0.18, 0.05}})
   Object('dynamic', 'block1', 175, 550, {0,0, 50,0, 50,100, 0,100}, {density=5})
   Object('dynamic', 'block2', 200, 400, {0,0, 100,0, 100,50, 0,50}, {density=2})
 
+  --graphics init
   love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
 
+  --input init
+  input = InputHandler:new()
 end
 
 --------------------UPDATE--------------------
 
 function love.update(dt)
+  input:update(dt)
   world:update(dt)
-
-
-  if love.keyboard.isDown("right") then
-    game.objects.triangle.body:applyForce(400, 0)
-  elseif love.keyboard.isDown("left") then
-    game.objects.triangle.body:applyForce(-400, 0)
-  end
 end
 
 --------------------DRAW--------------------
 function love.draw()
-
   for i, object in pairs(game.objects) do
     Graphics.draw(object)
   end
-
 end
 
 --------------------END--------------------
@@ -60,19 +57,37 @@ end
 --------------------CALLBACK FUNCTIONS--------------------
 
 function love.gamepadpressed(joystick, button)
+  input:startGamepad(joystick, button)
+end
 
+function love.gamepadreleased(joystick, button)
+  input:endGamepad(joystick, button)
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  if love.keyboard.isDown("escape") then
-    love.event.quit('Player Exited')
-  end
+  input:startKeyboard(key, isrepeat)
+
+  --if key == 'escape' then
+  --  love.event.quit('Player Exited')
+  --end
+  --if key == 'left' then
+--    game.objects.triangle.body:applyForce(-400, 0)
+--  end
+--  if key == 'right' then
+  --  game.objects.triangle.body:applyForce(400, 0)
+  --end
+
+
+end
+
+function love.keyreleased(key)
+  input:endKeyboard(key)
 end
 
 function love.joystickadded(joystick)
-
+  input:addGamepad(joystick)
 end
 
 function love.joystickremoved(joystick)
-
+  input:removeGamepad(joystick)
 end
